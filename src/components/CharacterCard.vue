7d840d89593b9953abda8cb2970a6dc1d50488f5
dtylam <8455943+dtylam@users.noreply.github.com>
2020-02-26 23:25:19 +0000

    small scaffolding of post detail

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list-item>
+              <v-list-item-avatar>
+                <span>{{character}}</span>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title></v-list-item-title>
+                <v-list-item-subtitle></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+</template>
+
+<script>
+export default {
+  name: "CharacterCard",
+  props: {
+    character: String
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/PostDetail.vue
+++ src/pages/PostDetail.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
     <PostCard :post="post" />
+    <v-row>
+    <v-col cols="12" sm="6" offset-sm="3">
+      <v-card>
+        <v-list two-line>
+          <!-- <template v-for="(item, index) in items.slice(0, 6)">
+            <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+            <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
+            
+          </template> -->
+        </v-list>
+      </v-card>
+    </v-col>
+  </v-row>
   </div>
 </template>
 
 <script>
 import PostCard from "../components/PostCard";
+import CharacterCard from "../components/CharacterCard";
 import constants from "../constants";
 
 export default {
