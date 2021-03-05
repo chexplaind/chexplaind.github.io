67e7635d32027d36390e7cc1a517e5f1a18a4903
dtylam <8455943+dtylam@users.noreply.github.com>
2021-03-05 02:04:34 +0000

    added store link

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,39 +1,68 @@
 <template>
   <div class="about-content">
     <h2>What happens when you literally translate words into Chinese?</h2>
-    <p>This is a collection of literal meanings of Chinese words for common places, objects, or phrases, eg.</p>
-    <v-chip-group class="tags-container">
-      <router-link :to="{ name: 'tag', params: { tagName: 'countries' }}">
-        <v-chip label link>Countries</v-chip>
-      </router-link>
-      <router-link :to="{ name: 'tag', params: { tagName: 'cities' }}">
-        <v-chip label link>Cities</v-chip>
-      </router-link>
-      <router-link :to="{ name: 'tag', params: { tagName: 'food' }}">
-        <v-chip label link>Food</v-chip>
-      </router-link>
-      <router-link :to="{ name: 'tag', params: { tagName: 'china' }}">
-        <v-chip label link>China</v-chip>
-      </router-link>
-      <router-link :to="{ name: 'tag', params: { tagName: 'europe' }}">
-        <v-chip label link>Europe</v-chip>
-      </router-link>
-      <router-link :to="{ name: 'tag', params: { tagName: 'asia' }}">
-        <v-chip label link>Asia</v-chip>
-      </router-link>
-    </v-chip-group>
+    <p>
+      This is a collection of literal meanings of Chinese words for common
+      places, objects, or phrases, eg.
+      <v-chip-group class="tags-container">
+        <router-link :to="{ name: 'tag', params: { tagName: 'countries' } }">
+          <v-chip label link>Countries</v-chip>
+        </router-link>
+        <router-link :to="{ name: 'tag', params: { tagName: 'cities' } }">
+          <v-chip label link>Cities</v-chip>
+        </router-link>
+        <router-link :to="{ name: 'tag', params: { tagName: 'food' } }">
+          <v-chip label link>Food</v-chip>
+        </router-link>
+        <router-link :to="{ name: 'tag', params: { tagName: 'china' } }">
+          <v-chip label link>China</v-chip>
+        </router-link>
+        <router-link :to="{ name: 'tag', params: { tagName: 'europe' } }">
+          <v-chip label link>Europe</v-chip>
+        </router-link>
+        <router-link :to="{ name: 'tag', params: { tagName: 'asia' } }">
+          <v-chip label link>Asia</v-chip>
+        </router-link>
+      </v-chip-group>
+    </p>
     <p>
       These could be translated by
-      <router-link :to="{ name: 'tag', params: { tagName: 'E2C' }}">mimicking pronunciations</router-link>, coined by
-      <router-link :to="{ name: 'tag', params: { tagName: 'meaning' }}">meaning</router-link>, or original Chinese names
-      <router-link :to="{ name: 'tag', params: { tagName: 'C2E' }}">romanised into English</router-link>.
+      <router-link :to="{ name: 'tag', params: { tagName: 'E2C' } }"
+        >mimicking pronunciations</router-link
+      >, coined by
+      <router-link :to="{ name: 'tag', params: { tagName: 'meaning' } }"
+        >meaning</router-link
+      >, or original Chinese names
+      <router-link :to="{ name: 'tag', params: { tagName: 'C2E' } }"
+        >romanised into English</router-link
+      >.
+    </p>
+    <p>
+      Support our work and buy them as stickers and magnets:
+      <v-chip
+        href="https://www.redbubble.com/people/chexplaind/explore"
+        target="_blank"
+        rel="noreferrer"
+        color="red"
+        text-color="white"
+      >
+        <v-avatar left>
+          <v-icon>{{ storeIconPath }}</v-icon>
+        </v-avatar>
+        Store
+      </v-chip>
     </p>
   </div>
 </template>
 
 <script>
+import { mdiStoreOutline } from "@mdi/js";
+
 export default {
-  name: 'About',
+  name: "About",
+  data: () => ({
+    storeIconPath: mdiStoreOutline,
+  }),
 };
 </script>
 
