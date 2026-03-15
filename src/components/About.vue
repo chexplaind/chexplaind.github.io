08ddacb6a3dea7cae13d39a229d16c4bbe8fa260
Donald <8455943+dtylam@users.noreply.github.com>
2026-03-15 17:00:59 +0000

    fix about view on mobile

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about-content">
-    <PostCard :post="titlePost" aspect-ratio="2 / 1" />
+    <PostCard :post="titlePost" aspect-ratio="3 / 2" />
     <v-chip-group :style="{'margin': 'auto', 'max-width': '360px', 'padding-bottom': '16px'}">
       <v-chip
         href="https://www.redbubble.com/people/chexplaind/explore"
